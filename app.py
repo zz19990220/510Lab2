942ded49dd37c5548015eab256197d542d01b9fd
zz19990220 <116690246+zz19990220@users.noreply.github.com>
2024-04-03 07:00:54 +0800

    Update app.py

--- app.py
+++ app.py
@@ -1,1 +1,7 @@
+import streamlit as st
 
+st.set_page_config(
+    page_title="Zeng Zhang - Designer, Landscape Architect",
+    page_icon="👾",
+    layout="centered",
+    initial_sidebar_state="auto",
